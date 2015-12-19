54912e1da0401f189f82618e0988040853526613
Ben Scott <ben@schmurgon.net>
2015-12-19 21:47:15 +1100

    Initial commit

---
+++ FlickFindr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickFindr
+//
+//  Created by Ben Scott on 19/12/2015.
+//  Copyright © 2015 Schmurgon Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
